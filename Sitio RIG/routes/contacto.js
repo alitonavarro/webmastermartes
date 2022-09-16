2ccb974e203187d777471d78f82a3561b469736a
Alejandro Navarro <alito_navarro@hotmail.com>
2022-09-16 20:53:43 -0300

    Tarea M4U3 - Sitio RIG

---
+++ Sitio RIG/routes/contacto.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('contacto', {
+    isContacto: true
+  }); // contacto.hbs
+});
+
+router.post('/', async (req, res, next) => {
+
+  // console.log(req.body)
+
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'alito_navarro@hotmail.com',
+    subject: 'CONTACTO WEB',
+    html: nombre + " se contacto a a través de la web y quiere más informacion a este correo: " + email + ". <br> Además, hizo este comentario : " + mensaje + ". <br> Su tel es: " + tel
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+  });
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('contacto', {
+    isContacto: true,
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+module.exports = router;
